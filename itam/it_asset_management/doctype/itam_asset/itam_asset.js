a580f4305065aa477504fc27d225678b7ce941bc
Kieran Smith <kieran.smith@res-it.solutions>
2021-07-10 06:32:27 +0000

    Add initial doctypes

---
+++ itam/it_asset_management/doctype/itam_asset/itam_asset.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Res-IT Solutions Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ITAM Asset', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ itam/it_asset_management/doctype/itam_asset/test_itam_asset.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Res-IT Solutions Ltd. and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestITAMAsset(unittest.TestCase):
+	pass
