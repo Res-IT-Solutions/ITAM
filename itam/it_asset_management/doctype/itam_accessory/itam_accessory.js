70a5ab7da17395d8c2b7adb7954b104dd9f03c91
Kieran Smith <kieran.smith@res-it.solutions>
2021-07-13 09:43:26 +0000

    doctype chages

---
+++ itam/it_asset_management/doctype/itam_accessory/itam_accessory.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Res-IT Solutions Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('ITAM Accessory', {
+	// refresh: function(frm) {
+
+	// }
+});
